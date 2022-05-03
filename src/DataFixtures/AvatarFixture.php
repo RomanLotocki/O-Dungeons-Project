efa61adf2f4f989c21d53dad13378d7eb6223799
RomanLotocki <roman.lotocki@gmail.com>
2022-05-03 14:09:15 +0200

    feat : avatarfixtures

---
+++ src/DataFixtures/AvatarFixture.php
@@ -0,0 +1,29 @@
+<?php
+
+namespace App\DataFixtures;
+
+use Faker\Factory;
+use App\Entity\Avatar;
+use Doctrine\Persistence\ObjectManager;
+use Doctrine\Bundle\FixturesBundle\Fixture;
+
+class AvatarFixture extends Fixture
+{
+    public function load(ObjectManager $manager): void
+    {
+        $faker = Factory::create('fr_FR');
+
+        for ($i=0; $i < 10; $i++) {
+            $avatar = new Avatar();
+            $avatar->setName($faker->lastName());
+            $avatar->setImageUrl($faker->url());
+
+            $manager->persist($avatar);
+        }
+        
+        // $product = new Product();
+        // $manager->persist($product);
+
+        $manager->flush();
+    }
+}
