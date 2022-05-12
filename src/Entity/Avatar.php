d2bf059dfa557ccd18e551b1e7cf2f7f92bab39b
Pierre-ArnaudLandoin <palandoin@gmail.com>
2022-05-12 13:14:54 +0200

    feat: BREAD on Avatar in backoffice

--- src/Entity/Avatar.php
+++ src/Entity/Avatar.php
@@ -7,6 +7,7 @@ use Doctrine\Common\Collections\ArrayCollection;
 use Doctrine\Common\Collections\Collection;
 use Doctrine\ORM\Mapping as ORM;
 use Symfony\Component\Serializer\Annotation\Groups;
+use Symfony\Component\Validator\Constraints as Assert;
 
 /**
  * @ORM\Entity(repositoryClass=AvatarRepository::class)
@@ -26,6 +27,7 @@ class Avatar
      * @ORM\Column(type="string", length=255)
      * @Groups("browse_avatars")
      * @Groups("read_user")
+     * @Assert\NotBlank
      */
     private $name;
 
@@ -33,6 +35,7 @@ class Avatar
      * @ORM\Column(type="string", length=255)
      * @Groups("browse_avatars")
      * @Groups("read_user")
+     * @Assert\Url
      */
     private $imageUrl;
 
