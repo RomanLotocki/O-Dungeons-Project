ce67e360f26973ed80ae3eaaa75f62c623f8c335
Pierre-ArnaudLandoin <palandoin@gmail.com>
2022-05-13 11:34:56 +0200

    feat: BREAD on User

--- src/Controller/Admin/PlayableClassController.php
+++ src/Controller/Admin/PlayableClassController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/admin/playable/class")
+ * @Route("/admin/classe")
  */
 class PlayableClassController extends AbstractController
 {
--- src/Controller/Admin/SubclassController.php
+++ src/Controller/Admin/SubclassController.php
@@ -12,7 +12,7 @@ use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
 
 /**
- * @Route("/admin/subclass")
+ * @Route("/admin/sous-classe")
  */
 class SubclassController extends AbstractController
 {
